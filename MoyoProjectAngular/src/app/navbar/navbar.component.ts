3d4f1e73fa08d07f78cc4d8ad874ffb6dfb887f6
TTollig <127200928+TTollig@users.noreply.github.com>
2024-07-25 23:28:31 +0200

    NavBar Working Sorta

---
+++ MoyoProjectAngular/src/app/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  get isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
+
+  get userRole() {
+    return this.authService.getRole();
+  }
+}
